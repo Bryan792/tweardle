927484109aac274e3109787989e136ee26ce6aa2
Bryan792 <Bryan792@gmail.com>
2022-03-28 04:12:51 +0000

    Deploying to gh-pages from @ Bryan792/tweardle@e4fa5d14e97907849f1e9940257be60b42de61df 🚀

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-//import '../styles/globals.css'
-import 'bootswatch/dist/darkly/bootstrap.min.css'
-import 'react-bootstrap-typeahead/css/Typeahead.css';
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
--- pages/api/sotd.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-import fs from 'fs'
-
-let songs = JSON.parse(fs.readFileSync('songs.json'))
-
-export default function handler(req, res) {
-  res.status(200).json(songs[Math.floor(Math.random() * songs.length)])
-}
